de2629fe51479f776ba0ba963371d67f1602c074
RyanBoring <boring.ryan.alan@gmail.com>
2019-06-01 13:40:58 -0400

    Begin work

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,13 @@ import java.util.Vector;
 
 public class Main
 {
+    public static double dt_countsPerSwerveRotation = 4096;
+    public static int[] driveMultipliers = {1, 1, 1, 1};
+
     public static void main(String[] args)
     {
+        int joystickNumber = 0;//1;
+
         RenderWindow window = new RenderWindow(new VideoMode(800, 800), "Swerve Drive Visualization");
         window.setFramerateLimit(120);
 
@@ -30,7 +35,7 @@ public class Main
         Wheel bottomLeft = new Wheel(arrowTexture, new Vector2f(200, 600), robotCenter);
         Wheel bottomRight = new Wheel(arrowTexture, new Vector2f(600, 600), robotCenter);
 
-        double leftX, leftY, rightX;
+        double leftX, leftY, rotationMagnitude;
 
         while (window.isOpen())
         {
@@ -42,9 +47,11 @@ public class Main
                 }
             }
 
-            leftX = -Joystick.getAxisPosition(0, Joystick.Axis.X) / 100;
-            leftY = -Joystick.getAxisPosition(0, Joystick.Axis.Y) / 100;
-            rightX = -Joystick.getAxisPosition(0, Joystick.Axis.U) / 100;
+            leftX = -Joystick.getAxisPosition(joystickNumber, Joystick.Axis.X) / 100;
+            leftY = -Joystick.getAxisPosition(joystickNumber, Joystick.Axis.Y) / 100;
+            rotationMagnitude = -Joystick.getAxisPosition(joystickNumber, Joystick.Axis.U) / 100;
+
+            System.out.println(leftX);
 
             /*
             System.out.println("Left X: " + leftX);
@@ -65,10 +72,10 @@ public class Main
              */
 
             Vector<Vector2f> wheelVectors = new Vector<>();
-            wheelVectors.add(Vector2f.add(movement, new Vector2f((float) (rightX * Math.cos(topLeft.getPerpendicularAngle())), (float) (rightX * Math.sin(topLeft.getPerpendicularAngle())))));
-            wheelVectors.add(Vector2f.add(movement, new Vector2f((float) (rightX * Math.cos(topRight.getPerpendicularAngle())), (float) (rightX * Math.sin(topRight.getPerpendicularAngle())))));
-            wheelVectors.add(Vector2f.add(movement, new Vector2f((float) (rightX * Math.cos(bottomLeft.getPerpendicularAngle())), (float) (rightX * Math.sin(bottomLeft.getPerpendicularAngle())))));
-            wheelVectors.add(Vector2f.add(movement, new Vector2f((float) (rightX * Math.cos(bottomRight.getPerpendicularAngle())), (float) (rightX * Math.sin(bottomRight.getPerpendicularAngle())))));
+            wheelVectors.add(Vector2f.add(new Vector2f(driveMultipliers[0] * movement.x, driveMultipliers[0] * movement.y), new Vector2f((float) (rotationMagnitude * Math.cos(topLeft.getPerpendicularAngle())), (float) (rotationMagnitude * Math.sin(topLeft.getPerpendicularAngle())))));
+            wheelVectors.add(Vector2f.add(new Vector2f(driveMultipliers[1] * movement.x, driveMultipliers[1] * movement.y), new Vector2f((float) (rotationMagnitude * Math.cos(topRight.getPerpendicularAngle())), (float) (rotationMagnitude * Math.sin(topRight.getPerpendicularAngle())))));
+            wheelVectors.add(Vector2f.add(new Vector2f(driveMultipliers[2] * movement.x, driveMultipliers[2] * movement.y), new Vector2f((float) (rotationMagnitude * Math.cos(bottomLeft.getPerpendicularAngle())), (float) (rotationMagnitude * Math.sin(bottomLeft.getPerpendicularAngle())))));
+            wheelVectors.add(Vector2f.add(new Vector2f(driveMultipliers[3] * movement.x, driveMultipliers[3] * movement.y), new Vector2f((float) (rotationMagnitude * Math.cos(bottomRight.getPerpendicularAngle())), (float) (rotationMagnitude * Math.sin(bottomRight.getPerpendicularAngle())))));
 
             double largestMagnitude = 1;
             for (Vector2f vector : wheelVectors)
@@ -90,14 +97,7 @@ public class Main
                 }
             }
 
-            double multiplier;
-            if(largestMagnitude > 1)
-            {
-                multiplier = 1 / largestMagnitude;
-            } else
-            {
-                multiplier = 1;
-            }
+            double multiplier = largestMagnitude > 1 ? 1 / largestMagnitude : 1;
 
             /*
             We want to set each vector to a value that is a fraction of the largest vector to normalize them
@@ -109,10 +109,15 @@ public class Main
                wheelVectors.setElementAt(new Vector2f((float) (wheelVectors.elementAt(i).x * multiplier), (float) (wheelVectors.elementAt(i).y * multiplier)), i);
             }
 
-            topLeft.applyRotation(wheelVectors.elementAt(0));
-            topRight.applyRotation(wheelVectors.elementAt(1));
-            bottomLeft.applyRotation(wheelVectors.elementAt(2));
-            bottomRight.applyRotation(wheelVectors.elementAt(3));
+            turnToAngle(topLeft, Math.atan2(wheelVectors.get(0).y, wheelVectors.get(0).x) * 180 / Math.PI, 0);
+            turnToAngle(topRight, Math.atan2(wheelVectors.get(1).y, wheelVectors.get(1).x) * 180 / Math.PI, 1);
+            turnToAngle(bottomLeft, Math.atan2(wheelVectors.get(2).y, wheelVectors.get(2).x) * 180 / Math.PI, 2);
+            turnToAngle(bottomRight, Math.atan2(wheelVectors.get(3).y, wheelVectors.get(3).x) * 180 / Math.PI, 3);
+
+            topLeft.update();
+            topRight.update();
+            bottomLeft.update();
+            bottomRight.update();
 
             window.clear(Color.BLACK);
             window.draw(topLeft);
@@ -120,13 +125,62 @@ public class Main
             window.draw(bottomLeft);
             window.draw(bottomRight);
             window.display();
+
+            System.out.println();
         }
+    }
+
+    public static void turnToAngle(Wheel wheel, double angle, int index)
+    {
+        // This gives us the counts of the swivel as if rotating it 360 degrees looped its angle back to 0
+        int rotationsCompleted = (int) Math.abs(wheel.getSelectedSensorPosition(0) / dt_countsPerSwerveRotation);
+        double absoluteCounts = (Math.abs(wheel.getSelectedSensorPosition(0)) - (rotationsCompleted * dt_countsPerSwerveRotation));// / dt_countsPerSwerveRotation * 360;
+
+        double counts = angle / 360 * dt_countsPerSwerveRotation;
+
+        // Puts negative counts in positive terms
+        if (absoluteCounts < 0)
+        {
+            absoluteCounts = dt_countsPerSwerveRotation + absoluteCounts;
+        }
+
+        // We had to put the sensor position through Math.abs before, so this fixes absoluteCounts
+        if (wheel.getSelectedSensorPosition(0) < 0)
+        {
+            absoluteCounts = dt_countsPerSwerveRotation - absoluteCounts;
+        }
+
+        // This enables field-centric driving. It adds the rotation of the robot to the total counts so it knows where to turn
+        //mPigeon.getYawPitchRoll(ypr);
+        //absoluteCounts += ypr[0] * dt_countsPerSwerveRotation;
+
+        // If it's faster to turn to the angle opposite of the angle we were given and drive backwards, do that thing
+        if (counts - absoluteCounts > dt_countsPerSwerveRotation / 2)
+        {
+            counts = dt_countsPerSwerveRotation - counts;
+            driveMultipliers[index] = -1;
+        } else
+        {
+            driveMultipliers[index] = 1;
+        }
+
+        double difference = counts - absoluteCounts;
+
+        System.out.println("Counts: " + counts + " Abs: " + absoluteCounts);
+
+        wheel.set(wheel.getSelectedSensorPosition(0) + difference > 0 ? 1 : -1);
     }
 }
 
 class Wheel extends Sprite
 {
     private Vector2f robotCenter;
+
+    private double output = 0;
+    private double counts = 0;
+
+    private double delay = 0.005;
+    private double last = System.currentTimeMillis() / 1000.0;
 
     public Wheel(Texture texture, Vector2f position, Vector2f robotCenter)
     {
@@ -138,16 +192,24 @@ class Wheel extends Sprite
         this.robotCenter = robotCenter;
     }
 
-    public void applyRotation(Vector2f total)
+    void set(double output)
     {
-        setRotation((float) (Math.atan2(total.y, total.x) * 180 / Math.PI - 90));
+        this.output = output;
+    }
 
-        setScale(1, (float) Math.sqrt(Math.pow(total.x, 2) + Math.pow(total.y, 2)));
-
-        if (getScale().y < 0.2)
+    void update()
+    {
+        if (System.currentTimeMillis() / 1000.0 - last > delay)
         {
-            setScale(1, 0.2f);
+            rotate((float) output);
+            counts += output / 360 * 4096;
+            last = System.currentTimeMillis() / 1000.0;
         }
+    }
+
+    double getSelectedSensorPosition(int pidx)
+    {
+        return counts;
     }
 
     public double getRadius()
